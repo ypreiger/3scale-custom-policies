bc807a9fa638445e8eee28dca6593508e2e38074
ypreiger <ypreiger@redhat.com>
2020-07-20 23:44:12 +0300

    Update init.lua

--- policies/sendtoimvision/0.1/init.lua
+++ policies/sendtoimvision/0.1/init.lua
@@ -1,2 +1,2 @@
-package.path = package.path .. './src/policies/sendtoimvision/0.1/?.lua;/usr/local/share/lua/5.1/resty/?.lua'
+--package.path = package.path .. './src/policies/sendtoimvision/0.1/?.lua;/usr/local/share/lua/5.1/resty/?.lua'
 return require('sendtoimvision')
